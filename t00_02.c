f46e84e0553fd81238095a0156c442f6382d560c
JenniferSihotang <jennifersihotang04@gmail.com>
2024-01-15 11:17:27 +0700

    Jennifer

--- t00_02.c
+++ t00_02.c
@@ -1,9 +1,10 @@
-// NIM - NAMA
+// 12S23047 - Jennifer Hopenes Sihotang
 
 #include <stdio.h>
 
 int main(char **_argc, int _argv)
 {
-    // Show your code here ...
+    printf("Aloha!");
+    
     return 0;
 }
